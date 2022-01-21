e775c45c9a9903951bc4baefebbabc54e00fc932
Ian Deans <ideans715@gmail.com>
2022-01-20 22:23:57 -0800

    lotsa shit... should been more commits...

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import Breakdown from './Breakdown/Breakdown'
 import CornerData from './CornerData/CornerData'
+import DrawerCount from './DrawerCount/DrawerCount'
 import Employees from './Employees/Employees'
 import PaidOut from './PaidOut/PaidOut'
 import Shift from './Shift/Shift'
@@ -7,6 +8,7 @@ import Shift from './Shift/Shift'
 export {
     Breakdown,
     CornerData,
+    DrawerCount,
     Employees,
     PaidOut,
     Shift,
